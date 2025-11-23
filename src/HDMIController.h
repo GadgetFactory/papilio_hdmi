be6e69d317f839bc7d7b3709abab4502fb7bfe94
Jack Gassett <jack.gassett@gadgetfactory.net>
2025-11-23 12:08:34 -0700

    Fix naming conflict: rename COLOR_* to HDMI_COLOR_* to avoid conflicts with RGB LED enum

--- src/HDMIController.h
+++ src/HDMIController.h
@@ -37,22 +37,22 @@
 #define PATTERN_TEXT_MODE   0x03
 
 // Text colors (4-bit: [3]=bright, [2]=red, [1]=green, [0]=blue)
-#define COLOR_BLACK         0x00
-#define COLOR_BLUE          0x01
-#define COLOR_GREEN         0x02
-#define COLOR_CYAN          0x03
-#define COLOR_RED           0x04
-#define COLOR_MAGENTA       0x05
-#define COLOR_BROWN         0x06
-#define COLOR_LIGHT_GRAY    0x07
-#define COLOR_DARK_GRAY     0x08
-#define COLOR_LIGHT_BLUE    0x09
-#define COLOR_LIGHT_GREEN   0x0A
-#define COLOR_LIGHT_CYAN    0x0B
-#define COLOR_LIGHT_RED     0x0C
-#define COLOR_LIGHT_MAGENTA 0x0D
-#define COLOR_YELLOW        0x0E
-#define COLOR_WHITE         0x0F
+#define HDMI_COLOR_BLACK         0x00
+#define HDMI_COLOR_BLUE          0x01
+#define HDMI_COLOR_GREEN         0x02
+#define HDMI_COLOR_CYAN          0x03
+#define HDMI_COLOR_RED           0x04
+#define HDMI_COLOR_MAGENTA       0x05
+#define HDMI_COLOR_BROWN         0x06
+#define HDMI_COLOR_LIGHT_GRAY    0x07
+#define HDMI_COLOR_DARK_GRAY     0x08
+#define HDMI_COLOR_LIGHT_BLUE    0x09
+#define HDMI_COLOR_LIGHT_GREEN   0x0A
+#define HDMI_COLOR_LIGHT_CYAN    0x0B
+#define HDMI_COLOR_LIGHT_RED     0x0C
+#define HDMI_COLOR_LIGHT_MAGENTA 0x0D
+#define HDMI_COLOR_YELLOW        0x0E
+#define HDMI_COLOR_WHITE         0x0F
 
 class HDMIController {
 public:
